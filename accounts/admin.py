3f271e3b31797eb7f8857400d4deddcad1c1af4d
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-01-31 11:01:26 +0330

    make customuser model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from forms import CustomUserChangeForm, CustomUserCreationForm
+from models import CustomUser
 
-# Register your models here.
+user_model = get_user_model()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = user_model
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('username', 'email', '')
+
--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-class Customer(AbstractUser):
+class CustomUser(AbstractUser):
     pass
