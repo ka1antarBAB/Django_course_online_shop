9621879acc92bac45b80831cbde1e663f122481d
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-06 10:27:49 +0330

    make new model for products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    price = models.PositiveIntegerField()
+    price_daler = models.DecimalField(max_digits=4, decimal_places=2)
