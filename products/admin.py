592abeec1d1ea3c5ae1da2b2961750a948cce2f6
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-07 12:59:53 +0330

    make Comment model for product and set on Admin

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
 from accounts.models import CustomUser
-from products.models import Product
+from products.models import Product, Comment
 # Register your models here.
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    model = Product
     list_display = ('title', 'price', 'datetime_created', 'active',)
 
+
+@admin.register(Comment)
+class ProductCommentAdmin(admin.ModelAdmin):
+    list_display = ('product', 'datetime_created', 'active',)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -17,4 +18,25 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    PRODUCT_START = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'not bad'),
+        ('4', 'good'),
+        ('5', 'very good'),
+
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
+
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+    point = models.CharField(choices=PRODUCT_START, max_length=10, default=PRODUCT_START[0][0], blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
 
