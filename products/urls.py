27f700f14f4e0dfd9ed504f0069aeb0bc51bb9d4
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-12 08:27:39 +0330

    delete test section

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.ProductListView.as_view(), name='list'),
     path('<int:pk>/', views.ProductDetailView.as_view(), name='detail'),
     path('comment/<int:product_id>/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('test/', views.test_translation, name='test'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -46,34 +46,3 @@ class CommentCreateView(generic.CreateView):
 
         obj.product = product
         return super().form_valid(form)
-
-
-def test_translation(request):
-    message = _('this is a success message')
-    messages.warning(request, message)
-    messages.error(request, message)
-    messages.success(request, message)
-    return render(request, 'products/message_test.html')
-
-
-# @login_required
-# def book_detail_view(request, pk):
-#     #book
-#     book = get_object_or_404(Book, pk=pk)
-#     comments = book.comments.all()
-#     if request.method == 'POST':
-#         comment_form = CommentForm(request.POST)
-#         if comment_form.is_valid():
-#             comment = comment_form.save(commit=False)
-#             comment.book = book #book
-#             comment.user = request.user
-#             comment.save()
-#             comment_form = CommentForm()
-#             redirect_url = reverse_lazy('books:book_detail')
-#     if request.method == 'GET':
-#         comment_form = CommentForm()
-#     return render(request, 'books/book_detail.html', context={
-#         'book': book,
-#         'comments': comments,
-#         'comment_form': comment_form
-#     })
