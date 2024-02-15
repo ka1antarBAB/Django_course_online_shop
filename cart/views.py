b1c04b0b7517db64d9b6de3038ab3b802e36ad67
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-15 12:03:35 +0330

    create view by name remove_from_cart

--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,11 @@ from . forms import AddToCartProductForm
 # Create your views here.
 def cart_detail_view(request):
     cart = Cart(request)
-
+    for item in cart:
+        item["product_update_quantity_form"] = AddToCartProductForm(initial={
+            "quantity": item["quantity"],
+            "ingredients": True,
+        })
     return render(request, 'cart/cart_detail.html', {'cart': cart})
 
 
@@ -21,6 +25,13 @@ def cart_add_view(request, product_id):
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data.get("quantity")
-        cart.add(product, quantity)
+        cart.add(product, quantity, replace_current_quantity=cleaned_data.get("inplace"))
 
+    return redirect('cart:detail_cart')
+
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
     return redirect('cart:detail_cart')
