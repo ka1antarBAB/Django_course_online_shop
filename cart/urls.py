6ecd903151977f84cf068e0c56283cafa4ffdfec
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-15 12:02:41 +0330

    create new path for remove_from_cart view

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail_view, name='detail_cart'),
-    path('add/<int:product_id>', views.cart_add_view, name='add_to_cart'),
-
+    path('add/<int:product_id>', views.cart_add_view, name='cart_add'),
+    path('remove/<int:product_id>', views.remove_from_cart, name='cart_remove'),
 
 ]
