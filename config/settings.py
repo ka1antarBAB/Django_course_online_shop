41bf575a359d7919b436376c57c16836782fb9ba
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-17 12:08:08 +0330

    translate the messages

--- config/settings.py
+++ config/settings.py
@@ -151,8 +151,8 @@ EMAIL_HOST_PASSWORD = 'fdht gywo lfac zziz'
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'fa-ir'
 TIME_ZONE = 'Asia/Tehran'
 
 LANGUAGES = (
