cd94dce4f13b85f3eb1ef4de6417adcf589a0531
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-22 11:44:23 +0330

    create new model in order app for orderitem

--- orders/models.py
+++ orders/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from phonenumber_field.modelfields import PhoneNumberField
 
+from products.models import Product
+
+
 # Create your models here.
 
 
 class Order(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     is_paid = models.BooleanField(default=False)
 
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = PhoneNumberField()
@@ -16,3 +19,21 @@ class Order(models.Model):
 
     date_ordered_created = models.DateTimeField(auto_now_add=True)
     date_ordered_modified = models.DateTimeField(auto_now=True)
+
+    order_notes = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f'Order : {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_item')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    date_ordered_created = models.DateTimeField(auto_now_add=True)
+    date_ordered_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order Item : {self.id} - {self.product} | Quantity : {self.quantity} | Price : {self.price} = ({self.quantity} * {self.price})'
