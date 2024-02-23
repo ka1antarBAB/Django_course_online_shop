73d41153ac918714e7384d5d8d8589ce0f87f623
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-23 10:05:20 +0330

    create OrderItemAdmin OrderAdmin and set desplay

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from jalali_date.admin import ModelAdminJalaliMixin
+
+from .models import Order, OrderItem
+
+
 # Register your models here.
+
+
+class OrderItemInLine(admin.StackedInline):
+    model = OrderItem
+    fields = ('order', 'product', 'quantity', 'price', )
+    extra = 1
+
+
+@admin.register(Order)
+class OrderItemAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    model = Order
+    list_display = ('user', 'first_name', 'last_name', 'phone_number', 'date_ordered_created', 'is_paid',)
+    inlines = [OrderItemInLine]
+
+
+@admin.register(OrderItem)
+class OrderAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'price',)
