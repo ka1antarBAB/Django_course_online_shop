bdcbe7d22d646a00187b8dbddec7981456904759
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-23 10:51:04 +0330

    make simple view for order_create template

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
+
+
+class OrderListView(generic.TemplateView):
+    template_name = 'orders/order_create.html'
