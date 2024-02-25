6b49e95e6996317c2fd4d48bc3ead64a707bffd7
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-25 12:36:16 +0330

    . update urls

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='list'),
     path('<int:pk>/', views.ProductDetailView.as_view(), name='detail'),
