48693bfc4b8a86534489f80e580d449c9e7ccf00
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-25 12:29:06 +0330

    save products in request for orderitem

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+
 # Create your views here.
+from cart.cart import Cart
 from .forms import OrderForm
+from . models import OrderItem
 
 
+@login_required()
 def order_create_view(request):
-    return render(request, 'orders/order_create.html', {'form': OrderForm()})
+    form = OrderForm()
+    cart = Cart(request)
+    if len(cart) == 0:
+        messages.warning(request, _('you can not continue with your cart because your cart is empty '))
+        return redirect("list")
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        # order
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
+            # order item
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item["product_object"],
+                                         quantity=item["quantity"],
+                                         price=item["product_object"].price
+                                         )
+            cart.clear()
+            request.user.first_name = order.first_name
+            request.user.last_name = order.last_name
+            request.user.save()
+            messages.success(request, _('Your order has been successfully placed'))
+    else:
+        form = OrderForm()
+    return render(request, 'orders/order_create.html', context={
+        'form': form,
+
+    })
