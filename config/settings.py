9e716f3fc31821a2baffe60525cff768bd1f709a
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-02-27 11:25:31 +0330

    add env

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kalantarBAB83@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdht gywo lfac zziz'
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 
 # Internationalization
