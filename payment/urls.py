7e127eab3bf6382c86af4157e331d692a4731d10
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-03-06 13:15:29 +0330

    create new path for callback in payment/urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', views.payment_process, name='payment_process'),
+    path('process/', views.payment_process_view, name='payment_process'),
+    path('callback/', views.payment_callback_view, name='payment_callback'),
 
 ]
