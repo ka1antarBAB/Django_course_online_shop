570cf4b06cc952ace19d5ffbb788bc6d25488193
ka1antarBAB <EhsanMalekBS@gmail.com>
2024-03-07 16:20:55 +0330

    add new path for payment-process/callback sandbox

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,7 @@ app_name = 'payment'
 urlpatterns = [
     path('process/', views.payment_process_view, name='payment_process'),
     path('callback/', views.payment_callback_view, name='payment_callback'),
+    path('process-sandbox/', views.payment_process_sandbox_view, name='payment_process_sandbox'),
+    path('callback-sandbox/', views.payment_callback_sandbox_view, name='payment_callback_sandbox')
 
 ]
